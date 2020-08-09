ee28dd279cbfa5a7efa89eab91a0ee395dec62b1
Ruslan Bakeyev <bakeyevrus@MacBook-Pro.local>
2020-08-09 15:21:04 +0200

    Add script for project page

---
+++ projects.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', function () {
+    enableProjectFilters();
+
+    function enableProjectFilters() {
+        var filterLabelEl = document.querySelector('[data-project-filter-label]');
+        if (filterLabelEl == null) {
+            throw new Error("DOM element with attribute 'data-project-filter-label' doesn't exist");
+        }
+        var projects = document.querySelectorAll('.sec-project__item');
+
+        var pageDefaultFilter = 'all projects';
+        var activeFilter = getUrlParameterByName('filter') || pageDefaultFilter;
+
+        if (activeFilter !== pageDefaultFilter) {
+            filterProjects(activeFilter);
+        }
+
+        document.querySelectorAll('[data-project-filter]').forEach(function (el) {
+            var filterValue = el.getAttribute('data-project-filter');
+            el.addEventListener('click', onFilterClick(filterValue));
+        });
+
+        function getUrlParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+
+        function onFilterClick(filterValue) {
+            return function () {
+                if (activeFilter === filterValue) {
+                    return;
+                }
+                activeFilter = filterValue;
+                filterProjects(filterValue);
+            }
+        }
+
+        function filterProjects(filter) {
+            filterLabelEl.innerText = filter;
+            filter = filter.toLowerCase();
+            if (filter === pageDefaultFilter) {
+                projects.forEach(function (projectEl) { projectEl.style.display = 'flex'; });
+                return;
+            }
+
+            projects.forEach(function (projectEl) {
+                var projectCategoriesStr = projectEl.querySelector('[data-project-categories]').getAttribute('data-project-categories');
+                var shouldBeDisplayed = projectCategoriesStr != null && projectCategoriesStr.toLowerCase().includes(filter.toLowerCase());
+                projectEl.style.display = shouldBeDisplayed ? 'flex' : 'none';
+            });
+        }
+    }
+});
